88435106af7c495da19c8cf088aac3df6eddd031
haydar9 <haydar.iss4@gmail.com>
2020-04-30 21:59:14 +0000

    added query result hints for custom jpa query in bookrepository

--- src/main/java/com/example/cachexplore/BookRepository.java
+++ src/main/java/com/example/cachexplore/BookRepository.java
@@ -1,9 +1,13 @@
 package com.example.cachexplore;
 
+import javax.persistence.QueryHint;
+
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.CrudRepository;
 
-public interface BookRepository extends CrudRepository<Book, Long>{
+public interface BookRepository extends CrudRepository<Book, Long> {
 
-  Book findByIsbn(String isbn);
+    @QueryHints(value = { @QueryHint(name = "org.hibernate.cacheable", value = "true")})
+    Book findByIsbn(String isbn);
 
 }
