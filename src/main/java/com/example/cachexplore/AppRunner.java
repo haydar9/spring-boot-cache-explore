fc7bfcadf32115ac2fa8b1837010f53a3948e0b9
haydar9 <haydar.iss4@gmail.com>
2020-05-05 19:23:09 +0000

    organized AppRunner test cases and added batching to insert statements

--- src/main/java/com/example/cachexplore/AppRunner.java
+++ src/main/java/com/example/cachexplore/AppRunner.java
@@ -28,40 +28,46 @@ public class AppRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        List<Book> books = new ArrayList<>();
-        for(int i = 0; i < 10; i++) {
-            Book book = new Book("isbn-"+i, "some book");
-            //books.add(book);
-            bookRepository.save(book);
-        }
-        //bookRepository.saveAll(books);
-        
+        setUpTestSaveSomeBooks();
+
+        //testFindById();
+
+        testFindAllById();
+
+        testQueryCache();
+
+    }
+
+    private void testFindById() {
         logger.info("find ID 10 (try 1)");
         bookRepository.findById(10l);
         logger.info("find ID 10 (try 2)");
         bookRepository.findById(10l);
-        
-
-        logger.info("find 1,2,3,4,5 (try 1)");
-        bookRepository.findAllById(Arrays.asList(1l,2l,3l,4l,5l));
-        logger.info("find 1,2,3,4,5 (try 2)");
-        bookRepository.findAllById(Arrays.asList(1l,2l,3l,4l,5l));
-        
-        logger.info("find 1,2,3,4,5,6,7 (try 1)");
-        bookRepository.findAllById(Arrays.asList(1l,2l,3l,4l,5l));
-        
-        // logger.info("saving book");
-        // bookRepository.save(book);
-        // logger.info("find by id 1");
-        // bookRepository.findById(1L);
-        // logger.info("find by id 1 (2nd try)");
-        // bookRepository.findById(1L);
-        
-        // logger.info("find by isbn 1234 (try 1)");
-        // bookRepository.findByIsbn("isbn-1234");
-        // logger.info("find by isbn 1234 (try 2)");
-        // bookRepository.findByIsbn("isbn-1234");
-      
     }
 
+    private void setUpTestSaveSomeBooks() {
+        List<Book> books = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Book book = new Book("isbn-" + i, "some book");
+            books.add(book);
+        }
+        bookRepository.saveAll(books);
+    }
+
+    private void testQueryCache() {
+         logger.info("find by isbn 1234 (try 1)");
+        bookRepository.findByIsbn("isbn-1234");
+        logger.info("find by isbn 1234 (try 2)");
+        bookRepository.findByIsbn("isbn-1234");
+    }
+
+    private void testFindAllById() {
+        logger.info("find 1,2,3,4,5 (try 1)");
+        bookRepository.findAllById(Arrays.asList(1l, 2l, 3l, 4l, 5l));
+        logger.info("find 1,2,3,4,5 (try 2)");
+        bookRepository.findAllById(Arrays.asList(1l, 2l, 3l, 4l, 5l));
+
+        logger.info("find 1,2,3,4,5,6,7 (try 1)");
+        bookRepository.findAllById(Arrays.asList(1l, 2l, 3l, 4l, 5l, 6l, 7l));
+    }
 }
--- src/main/java/com/example/cachexplore/BookRepository.java
+++ src/main/java/com/example/cachexplore/BookRepository.java
@@ -4,15 +4,18 @@ import java.util.List;
 
 import javax.persistence.QueryHint;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.QueryHints;
 
 //public interface BookRepository extends CrudRepository<Book, Long> {
 public interface BookRepository extends JpaRepository<Book, Long> {
-    @QueryHints(value = { @QueryHint(name = "org.hibernate.cacheable", value = "true")})
+    //@QueryHints(value = { @QueryHint(name = "org.hibernate.cacheable", value = "true")})
+    @Cacheable(value="com.example.cachexplore.Book", key="#isbn")
     Book findByIsbn(String isbn);
 
     @Override
-    @QueryHints(value = { @QueryHint(name = "org.hibernate.cacheable", value = "true")})
+    //@QueryHints(value = { @QueryHint(name = "org.hibernate.cacheable", value = "true")})
+    @Cacheable(value="com.example.cachexplore.Book")
     List<Book> findAllById(Iterable<Long> ids);
 }
